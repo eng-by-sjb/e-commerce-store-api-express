37acc109aea104cb99320b4b7fff3c861bdffdcf
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2023-08-05 18:26:20 +0000

    Add skip and fields querying. Also update sort with a default sort

--- controllers/get-all-products.js
+++ controllers/get-all-products.js
@@ -1,10 +1,23 @@
 import Product from "../models/product.js";
 
 const getAllProducts = async (req, res, next) => {
-  const { _id, name, price, company, featured, rating, sort, limit } =
-    req.query;
+  const {
+    _id,
+    name,
+    price,
+    company,
+    featured,
+    rating,
+    sort,
+    limit,
+    fields,
+    skip,
+  } = req.query;
+
+  //Empty object which then assigned key value pairs
   const queryObject = {};
 
+  //assigning key value pairs if the property is in the req body
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
@@ -21,9 +34,17 @@ const getAllProducts = async (req, res, next) => {
   if (sort) {
     result = result.sort(sort.split(",").join(" "));
     // result = result.sort(sort.replaceAll(",", " "));
+  } else {
+    result = result.sort("createdAt");
   }
   if (limit) {
     result = result.limit(Number(limit));
+  }
+  if (fields) {
+    result = result.select(fields.split(",").join(" "));
+  }
+  if (skip) {
+    result = result.skip(Number(skip));
   }
 
   const products = await result;
